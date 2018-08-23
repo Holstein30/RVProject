507a5ee418f75f1600058514a35e94cb4d66bd9d
Zarnerz <holstein30@live.com>
2018-08-23 17:59:18 -0400

    some js psuedo

--- app/main.js
+++ app/main.js
@@ -1,1 +1,13 @@
 // Main JS Functionality
+
+// !Event Handlers
+// Navigation Events
+// Filter Events
+// Dealer Section Events
+// Connect Events
+// Footer Events
+
+// !Conditional List Rendering
+// Build HTML Elements
+// Render All
+// Filter Results
