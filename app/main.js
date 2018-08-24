dc76bb0d08b25c33cb78a990768c1d525ff05911
Dale Holstein <holstein30@live.com>
2018-08-24 17:47:20 -0400

    pulling data from dealers JSON

--- app/main.js
+++ app/main.js
@@ -8,6 +8,21 @@
 // Footer Events
 
 // !Conditional List Rendering
+function fetchData() {
+  fetch("./dealers.json").then(res => {
+    res.json().then(data => {
+      //update the rendered cards with the correct dealers based on the filter
+      renderData(data);
+    });
+  });
+}
+
+function renderData(data) {
+  let name = data.dealers[0].data.name;
+  document.getElementById("dealer-cards").innerHTML = name;
+}
+
+fetchData();
 // Build HTML Elements
 // Render All
 // Filter Results
